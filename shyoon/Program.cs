939a8ba2c7eb1fb65caadfc7778c010aeec3b696
윤성호 <hiptionjjang@gmail.com>
2022-03-18 22:55:35 +0900

    test PR

--- shyoon/Program.cs
+++ shyoon/Program.cs
@@ -10,7 +10,7 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! pr");
         }
     }
 }
